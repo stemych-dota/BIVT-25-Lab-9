adfec35baab9c19aefcdf644af4eee34227b5871
stemych-dota <s.temych@gmail.com>
2026-04-19 23:53:01 +0300

    Update Blue.cs

--- Lab9/Blue/Blue.cs
+++ Lab9/Blue/Blue.cs
@@ -1,3 +1,34 @@
-﻿namespace Lab9.Blue
+namespace Lab9.Blue
 {
+    public abstract class Blue
+    {
+        private string _input;
+
+        public string Input => _input;
+
+        protected Blue(string input)
+        {
+            _input = input;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+
+        protected static bool IsWordCharacter(char symbol)
+        {
+            return char.IsLetter(symbol) || symbol == '-' || symbol == '\'' || symbol == '`';
+        }
+
+        protected static bool IsWordBorderedByDigits(string text, int start, int endExclusive)
+        {
+            bool digitBefore = start > 0 && char.IsDigit(text[start - 1]);
+            bool digitAfter = endExclusive < text.Length && char.IsDigit(text[endExclusive]);
+            return digitBefore || digitAfter;
+        }
+    }
 }
